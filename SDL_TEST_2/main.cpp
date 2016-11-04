6dc2952334cd87569cdfc3d67a3d6a75d1e6b7d7
Richard-Lynch <Richard@richards-macbook-pro.local>
2016-11-04 19:20:35 +0000

    Initial Commit

---
+++ SDL_TEST_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_TEST_2
+//
+//  Created by Richard Lynch on 04/11/2016.
+//  Copyright © 2016 Lynch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
